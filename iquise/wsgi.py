f32fb191d8756fd536b913da4826fe4131d811a3
Michael Walsh <mpwalsh@mit.edu>
2018-08-20 12:58:49 -0400

    Beefed up wsgi.py

--- iquise/wsgi.py
+++ iquise/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iquise.settings")
 
 application = get_wsgi_application()
