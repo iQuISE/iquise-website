7c548a2ce8f35f3b749fff9cb3c3a47fc9f1eedb
Michael Walsh <mpwalsh@mit.edu>
2018-08-24 12:38:34 -0400

    1 day link to register users. No model update, just use URL hash with data.

--- website/static/website/assets/js/custom.js
+++ website/static/website/assets/js/custom.js
@@ -10,11 +10,15 @@ jQuery(document).ready(function(){
     });
     jQuery('.inlinechangelink').text('Details')
 
-    jQuery('.js-copylink').click(function(event) {
+    jQuery('.js-copylink').click(jscopy);
+});
+
+function jscopy(event) {
       // Select the email link anchor text
+      jQuery(this).append('<div id="active-coppy-wrapper"><textarea id="active-copy">'+jQuery(this).attr('data-copy')+'</textarea></div>');
       selection = window.getSelection();    // Save the selection.
       var range = document.createRange();
-      range.selectNode(this);
+      range.selectNode(jQuery('#active-copy')[0]);
       selection.removeAllRanges();          // Remove all ranges from the selection.
       selection.addRange(range);            // Add the new range.
       try {
@@ -30,9 +34,9 @@ jQuery(document).ready(function(){
       // Remove the selections - NOTE: Should use
       // removeRange(range) when it is supported
       window.getSelection().removeAllRanges();
+      jQuery('#active-coppy-wrapper').remove()
       return false; // prevent further callback (e.g. close side panel)
-});
-});
+}
 
 function tempAlert(target,msg,duration)
 {
