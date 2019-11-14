9eb1018b2778d2eaca96472af981fc41bcaf8f2e
Michael Walsh <mpwalsh@mit.edu>
2019-11-14 17:17:18 -0500

    added iquhack page

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -10,5 +11,6 @@ urlpatterns = [
     url(r'^join/$',views.join.as_view(), name='join'),
     url(r'^archive/$',views.archive, name='archive'),
     url(r'^register/(?P<hash>.*)/$', views.staff_register, name="register"),
-    url(r'^scheduler/$', views.scheduler, name='scheduler')
+    url(r'^scheduler/$', views.scheduler, name='scheduler'),
+    url(r'^(?i)iQuHACK/$', TemplateView.as_view(template_name='iquhack.html'), name='iquhack'),
 ]
