1c90f4a1f916e6411f33beb013bc8852688318d7
Michael Walsh <mpwalsh@mit.edu>
2019-11-26 18:52:49 -0500

    began adding unit tests

--- website/tests.py
+++ website/tests.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
+from .models import *
 
-# Create your tests here.
+class SeminarTodayBehavior(TestCase):
+    def setUp(self):
+        today = timezone.localtime()
+        # Setup the session
+        start = today - timedelta(months=6)
+        stop = today + timedelta(months=6)
+        session = Session.objects.create(title="TEST",
+                                         start=start,
+                                         stop=stop)
+        # Add a presenter
+        presenter = Presenter.objects.create(first_name='Foo',
+                                             last_name='Bar',
+                                             affiliation='MIT')
+        # Create an event slot in session
+        event = Event.objects.create(session=session
+                                     date=today)
+
+
+    def test_home_page(self):
+        pass
