9c260734a1ed446d9041eaed9f1727cf40c823b4
Michael Walsh <mpwalsh@mit.edu>
2019-12-08 15:47:27 -0500

    decreased screen 1280->1260 to include mac 12''

--- website/static/website/assets/js/main.js
+++ website/static/website/assets/js/main.js
@@ -8,7 +8,7 @@
 
 	skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
+		large: '(max-width: 1260px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)',
