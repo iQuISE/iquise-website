c8567899200bc51d7a4cd3cfe35c4fb7dc3c80a8
Walsh, Michael P <mpwalsh@hrl.com>
2020-12-05 12:38:32 -0500

    Linked template to views

--- iquhack/urls.py
+++ iquhack/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'iquhack'
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='iquhack.html'), name='index'),
+    url(r'^$', views.index, name='index'),
 ]
