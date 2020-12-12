a84b9cd8c50a122d919b10be3758e35b0b5286ca
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-12 14:21:59 -0500

    Add sponsor list in each hackathon page

--- iquhack/admin.py
+++ iquhack/admin.py
@@ -5,8 +5,13 @@ from django.contrib import admin
 
 from .models import Hackathon, Sponsor, Tier, CompanyContact
 
+class SponsorInline(admin.TabularInline):
+    model = Sponsor
+    extra = 1
+
 class HackathonAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "end_date", "published", "registration_open")
+    inlines = (SponsorInline, )
 
 class SponsorAdmin(admin.ModelAdmin):
     list_display = ("__unicode__", "hackathon", "company_contact")
