054ea62f30b5af6ec55b8208b4f2894aece0255c
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-12 16:31:32 -0500

    First pass at banner template replacement

--- iquhack/views.py
+++ iquhack/views.py
@@ -9,19 +9,42 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from iquhack.models import Hackathon, Tier, Sponsor
 
+# Taken from https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+
+def format_date_range(start, stop):
+    """Nicely format a date range without repeating month unless spans two.
+    
+    This ignores the year.
+
+    Example: February 1st-2nd; January 30th-February 1st.
+    """
+    # Ad ordinal
+    start_day = ordinal(start.day)
+    stop_day = start.strftime("%B %%s") % start_day
+
+    stop_day = ordinal(stop.day)
+    if start.month() != stop.month():
+        stop_day = stop.strftime("%B %%s") % stop_day
+    return "%s-%s" % (start_day, stop_day)
+
 # Create your views here.
 def index(request, start_date=None):
+    available_hackathons = Hackathon.objects.filter(published=True)
     if start_date:
         start_date = [int(item) for item in start_date.split("-")] # Convert to list of ints: [2020, 12, 12]
         start_date = datetime.date(*start_date)
         try:
-            hackathon = Hackathon.objects.get(start_date=start_date)
+            hackathon = available_hackathons.get(start_date=start_date)
         except ObjectDoesNotExist:
             raise Http404
     else: # Most recent
-        hackathon = Hackathon.objects.order_by("-start_date").first()
+        hackathon = available_hackathons.order_by("-start_date").first()
         if not hackathon: # No hackathons are in the database yet
             raise Http404 # TODO: probably shouldn't raise a 404 here
+    
+    # Easier to format the date here than in the template
+    formatted_event_date = format_date_range(hackathon.start_date, hackathon.end_date)
     
     # Prepare a template-friendly sponsor data: List of (tier, abs_height, (sponsors,)) tuples
     sponsors = []
@@ -35,6 +58,7 @@ def index(request, start_date=None):
 
     return render(request, "iquhack/iquhack.html", 
         context={
+            "formatted_event_date": formatted_event_date,
             "hackathon": hackathon,
             "sponsors": sponsors,
         })
