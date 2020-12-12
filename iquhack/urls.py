adbf0f909787bacd410d783457393dad9462fc11
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-12 14:47:44 -0500

    Set up URL for archive, send hackathon to html template

--- iquhack/urls.py
+++ iquhack/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+ISO_REGEX = r"[0-9]{4}-[0-9]{0,2}-[0-9]{0,2}"
+
 app_name = 'iquhack'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('^(?P<start_date>%s)$' % ISO_REGEX, views.index, name='index'),
 ]
