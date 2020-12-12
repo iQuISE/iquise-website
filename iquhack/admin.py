f7cf59789a9c52468ebfa405830cda1e1a4e2d76
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-12 17:24:23 -0500

    Remove currently and likely useless contacts, add sponsor stuff in (chose to use CSS still)

--- iquhack/admin.py
+++ iquhack/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Hackathon, Sponsor, Tier, CompanyContact
+from .models import Hackathon, Sponsor, Tier
 
 class SponsorInline(admin.TabularInline):
     model = Sponsor
@@ -22,9 +22,8 @@ class HackathonAdmin(admin.ModelAdmin):
     inlines = (SponsorInline, )
 
 class SponsorAdmin(admin.ModelAdmin):
-    list_display = ("__unicode__", "hackathon", "company_contact")
+    list_display = ("__unicode__", "hackathon")
 
 admin.site.register(Hackathon, HackathonAdmin)
 admin.site.register(Sponsor, SponsorAdmin)
 admin.site.register(Tier)
-admin.site.register(CompanyContact)
