181c0862f845f3d552b72889020ac1a4e4097468
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-12 14:47:44 -0500

    Set up URL for archive, send hackathon to html template

--- iquhack/views.py
+++ iquhack/views.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
 from django.shortcuts import render
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
+
+from iquhack.models import Hackathon
 
 # Create your views here.
-def index(request):
-    return render(request, "iquhack/iquhack.html")
+def index(request, start_date=None):
+    if start_date:
+        start_date = [int(item) for item in start_date.split("-")] # Convert to list of ints: [2020, 12, 12]
+        start_date = datetime.date(*start_date)
+        try:
+            hackathon = Hackathon.objects.get(start_date=start_date)
+        except ObjectDoesNotExist:
+            raise Http404
+    else: # Most recent
+        hackathon = Hackathon.objects.order_by("-start_date").first()
+    
+    return render(request, "iquhack/iquhack.html", context={"hackathon":hackathon})
