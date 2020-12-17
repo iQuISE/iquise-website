c27958c8189f3baf35dbebd15e425c6a8c2b17f6
Walsh, Michael P <mwalsh161@gmail.com>
2020-12-17 11:16:54 -0500

    script generalization

--- populate_data.py
+++ populate_data.py
@@ -1,38 +1,116 @@
-import json, os, django, datetime
+import os
+import json
+import datetime
+import pytz
+
+import django
+from django.db import transaction
+from django.core.files import File
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iquise.settings")
 django.setup()
 
 from iquhack.models import *
 
-hackathon = Hackathon(start_date = datetime.date(2021, 1, 29),
-end_date = datetime.date(2021, 1, 31),
-back_drop_image = "background_highres.png",
-published = True,
-opens = datetime.datetime.now(),
-deadline = datetime.date(2021, 1, 28)
-)
-hackathon.save()
+HACKATHONS = ["data2020.json"]
+TZ = pytz.timezone("EST")
 
-with open("data2020.json") as fid:
-    dat = json.load(fid)
+# atomic means if an error occurs at any point, we rollback
+@transaction.atomic
+def main(hackathon_dat_name):
+    with open(hackathon_dat_name, "r") as fid:
+        dat = json.load(fid)
 
-alltiers = []
-for i, tier in enumerate(dat["Tier"]):
-    t = Tier(index=i, logo_rel_size=tier["logo_rel_size"])
-    t.save()
-    alltiers.append(t)
+    hackathon_dat = dat["Hackathon"]
+    if Hackathon.objects.filter(start_date__year=hackathon_dat["start_date"][0]).count():
+        raise Exception("Hackathon this year already exists!")
 
-for i, FAQi in enumerate(dat["FAQ"]):
-    f = FAQ(question=FAQi["question"], answer=FAQi["answer"])
-    f.save()
-    UsedFAQ(hackathon=hackathon, FAQ=f, index=i).save()
+    print "Adding hackathon...",
+    # Update to appropriate python objects
+    hackathon_dat["start_date"] = datetime.date(*hackathon_dat["start_date"])
+    hackathon_dat["end_date"] = datetime.date(*hackathon_dat["end_date"])
+    hackathon_dat["back_drop_image"] = File(open(hackathon_dat["back_drop_image"], "r"))
+    hackathon_dat["opens"] = datetime.datetime(*hackathon_dat["opens"], tzinfo=TZ)
+    hackathon_dat["deadline"] = datetime.datetime(*hackathon_dat["deadline"], tzinfo=TZ)
+    # Add using kwarg notation
+    hackathon = Hackathon(**hackathon_dat)
+    hackathon.save()
+    print "done"
 
-for s in dat["Sponsor"]:
-    spons = Sponsor(name=s["name"], logo=s["logo"], link=s["link"])
-    spons.save()
-    Sponsorship(hackathon=hackathon, sponsor=spons, tier=alltiers[s["tier"]-1], platform = s["hardware"]).save()
+    # Prepare tiers
+    tiers_dat = {}
+    for tier_dat in dat["Tier"]:
+        tiers_dat[tier_dat["index"]] = tier_dat
 
-for i, s in enumerate(dat["Section"]):
-    sec = Section(title=s["title"], content=s["content"])
-    sec.save()
-    UsedSection(hackathon=hackathon, section=sec, index=i).save()
+    with open(dat["FAQs"], "r") as fid:
+        faqs_dat = fid.read().strip()
+
+    # Format of this file: {question}\n{answer; may contain \n}\n\n
+    faqs_dat = faqs_dat.split("\n\n")
+    print "Adding questions"
+    for i, faq_dat in enumerate(faqs_dat):
+        lines = faq_dat.strip().split("\n")
+        question = lines[0]
+        answer = "\n".join(lines[1:])  # Reassemble answer lines
+        print "  %s..." % question,
+        faq = FAQ(question=question, answer=answer)
+        faq.save()
+        UsedFAQ(hackathon=hackathon, FAQ=faq, index=i).save()
+        print "done"
+
+    print "Adding sponsors"
+    for sponsor_dat in dat["Sponsor"]:
+        sponsor, created = Sponsor.objects.get_or_create(
+            name=sponsor_dat["name"],
+            defaults = {
+                "logo": File(open(sponsor_dat["logo"], "r")),
+                "link": sponsor_dat["link"],
+            }
+        ) # this also saves it
+        if created:
+            print "  Created sponsor " + sponsor_dat["name"]
+        else:
+            print "  Found sponsor " + sponsor_dat["name"]
+        tier_index = sponsor_dat["tier"]
+        tier, created = Tier.objects.get_or_create(
+            index=tier_index,
+            # defaults = tiers_dat[tier_index] # would also work (just redundant with the index)
+            defaults={"logo_rel_size": tiers_dat[tier_index]["logo_rel_size"]},
+        )
+        if created:
+            print "  Created tier %i" % tier_index
+        else:
+            print "  Found tier %i" % tier_index
+        Sponsorship(
+            hackathon=hackathon,
+            sponsor=sponsor,
+            tier=tier,
+            platform=sponsor_dat["hardware"],
+        ).save()
+        print "    done."
+
+    print "Adding sections"
+    for i, section_dat in enumerate(dat["Section"]):
+        with open(section_dat["content"], "r") as fid:
+            content = fid.read().strip()
+        # TODO. Section -> SectionTemplate
+        print "  Adding %s..." % section_dat["title"],
+        section = Section(
+            title=section_dat["title"],
+            content=content,
+            general=section_dat.get("general", False),
+        )
+        section.save()
+        UsedSection(hackathon=hackathon, section=section, index=i).save()
+        print "done"
+
+
+if __name__ == "__main__":
+    original_cwd = os.getcwd()
+    root = os.path.dirname(__file__)
+    os.chdir(os.path.join(root, ".iquhack2021_temp"))
+    try:
+        for hackathon_dat_name in HACKATHONS:
+            main(hackathon_dat_name)
+    finally:
+        os.chdir(original_cwd)
