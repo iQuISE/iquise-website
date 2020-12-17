f88ea8f416afc7d455c3bee3f44331dbef72245e
Luke Qi <lukeqi@Lukes-MacBook-Pro.local>
2020-12-16 20:23:58 -0500

    removed delete(), added tiers to list

--- populate_data.py
+++ populate_data.py
@@ -4,7 +4,6 @@ django.setup()
 
 from iquhack.models import *
 
-Hackathon.objects.all().delete()
 hackathon = Hackathon(start_date = datetime.date(2021, 1, 29),
 end_date = datetime.date(2021, 1, 31),
 back_drop_image = "background_highres.png",
@@ -17,23 +16,22 @@ hackathon.save()
 with open("data2020.json") as fid:
     dat = json.load(fid)
 
-Tier.objects.all().delete()
+alltiers = []
 for i, tier in enumerate(dat["Tier"]):
-    Tier(index=i, logo_rel_size=tier["logo_rel_size"]).save()
+    t = Tier(index=i, logo_rel_size=tier["logo_rel_size"])
+    t.save()
+    alltiers.append(t)
 
-FAQ.objects.all().delete()
 for i, FAQi in enumerate(dat["FAQ"]):
     f = FAQ(question=FAQi["question"], answer=FAQi["answer"])
     f.save()
     UsedFAQ(hackathon=hackathon, FAQ=f, index=i).save()
 
-Sponsor.objects.all().delete()
 for s in dat["Sponsor"]:
     spons = Sponsor(name=s["name"], logo=s["logo"], link=s["link"])
     spons.save()
-    Sponsorship(hackathon=hackathon, sponsor=spons, tier=Tier.objects.filter(index=s["tier"]-1).first(), platform = s["hardware"], agreement="").save()
+    Sponsorship(hackathon=hackathon, sponsor=spons, tier=alltiers[s["tier"]-1], platform = s["hardware"]).save()
 
-Section.objects.all().delete()
 for i, s in enumerate(dat["Section"]):
     sec = Section(title=s["title"], content=s["content"])
     sec.save()
