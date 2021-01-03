0ae6efc7cd1d29aec41c3fb75bc6429b60434aeb
Walsh, Michael P <mwalsh161@gmail.com>
2021-01-03 16:21:23 -0500

    Adding in committee page; adds term model back

--- members/urls.py
+++ members/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^leadership/$', views.people, name='people_deprecated'), # deprecated, but don't delete so existing hyperlinks stil work
     url(r'^exec/$', views.people, name='people'),
     url(r'^join/$',views.join.as_view(), name='join'),
-    url(r'^register/(?P<hash>.*)/$', views.staff_register, name="register"),
-    url(r'^exec/(?P<user>.*)/$', views.staff_member, name="staff")
+    url(r'^register/(?P<hash_>.*)/$', views.staff_register, name="register"),
+    url(r'^exec/(?P<user>.*)/$', views.staff_member, name="staff"),
+    url(r'^c/(?P<name>.*)/$', views.committee, name="committee")
 ]
