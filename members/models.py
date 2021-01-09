f5d7c6de582e606beba4e0a8906903224d8cb606
Walsh, Michael P <mwalsh161@gmail.com>
2021-01-09 13:35:02 -0500

    Routed regular exec url, committee template almost done

--- members/models.py
+++ members/models.py
@@ -28,6 +28,15 @@ def get_current_term_start(*args, **kwargs):
 def get_active_term():
     return Term.objects.filter(start__lte=timezone.now().date()).first()
 
+def get_positions_held(group, term=None):
+    term = term or get_active_term()
+    positions = Position.objects.filter(committee=group)
+    posheld = PositionHeld.objects.filter(position__in=positions).filter(start__gte=term.start)
+    stop = term.get_end()
+    if stop:
+        posheld = posheld.filter(start__lt=stop)
+    return posheld
+
 class EmailIField(models.EmailField):
     # Case-insensitive email field
     def clean(self,*args,**kwargs):
@@ -95,19 +104,52 @@ def create_user_profile(sender, instance, created, **kwargs):
 class Committee(AlwaysClean):
     group = models.OneToOneField(Group, models.CASCADE)
     parent = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    description = models.TextField(
+        max_length=500,
+        blank=True,
+        help_text=(
+            "Short description here on first line (next line blank!)."
+            "<br><br>Elaborate here if you need."
+            "<br>Multiple lines if needed."
+        )
+    )
+    contact_email = models.EmailField(blank=True, help_text="If blank, parent's email will be used if available.")
+    show_email = models.BooleanField(default=True, help_text="If no self or parent Contact email; will not be displayed.")
+
+    @property
+    def short_description(self):
+        lines = [ln.strip() for ln in self.description.strip().splitlines()]
+        if not lines: # No text
+            return ""
+        try:
+            sep = lines.index("")
+        except ValueError: # No double newline
+            return ""
+        return " ".join(lines[:sep])
+
+    @property
+    def email(self):
+        # An email may be returned even if show_email=False!
+        this = self
+        while this:
+            if this.contact_email:
+                return this.contact_email
+            this = this.parent.committee
+        return None
 
     def clean(self):
-        parent = self.parent
-        while parent:
-            if self.group == parent:
+        ancestor = self.parent
+        while ancestor:
+            if self.group == ancestor:
                 raise ValidationError({"parent": "Parent cannot be the committee itself or any ancestor."})
-            parent = parent.committee.parent
+            ancestor = ancestor.committee.parent
 
     def __unicode__(self):
         return "%s info" % self.group
 
 # TODO: consider hiding explicit index, and use orderable UI: https://djangosnippets.org/snippets/1053/
 class Position(models.Model):
+    # TODO: committee should be renamed to "group" for less confusion
     DEFAULT_NAME = "" # Changing this will require updating old records too
     committee = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="positions")
     name = models.CharField(max_length=50)
@@ -179,6 +221,15 @@ class Term(models.Model):
     def is_active(self):
         return self.id == get_active_term().id
     is_active.boolean = True
+
+    def get_end(self):
+        next_term = Term.objects.filter(start__gt=self.start).first()
+        if next_term:
+            return next_term.start
+        return None
+
+    def __unicode__(self):
+        return self.start.isoformat()
 
     class Meta:
         ordering = ["-start"]
