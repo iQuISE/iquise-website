8d6399ecc2cb16ab50fdb18c3f1a7aed52b5503e
Walsh, Michael P <mwalsh161@gmail.com>
2021-01-10 17:25:27 -0500

    Generalized exec

--- members/urls.py
+++ members/urls.py
@@ -3,13 +3,10 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.models import Group
 
-# TODO: generalize this somehow and don't create!
-exec_, _ = Group.objects.get_or_create(name__iexact="exec")
-
 app_name = 'members'
 urlpatterns = [
-    url(r'^leadership/$', views.committee, {"name":exec_.name}, name='people_deprecated'), # deprecated, but don't delete so existing hyperlinks stil work
-    url(r'^exec/$', views.committee, {"name":exec_.name}, name='people'),
+    url(r'^leadership/$', views.committee, name='people_deprecated'), # deprecated, but don't delete so existing hyperlinks stil work
+    url(r'^exec/$', views.committee, name='people'),
     url(r'^join/$',views.join.as_view(), name='join'),
     url(r'^register/(?P<hash_>.*)/$', views.staff_register, name="register"),
     url(r'^staff/(?P<user>.*)/$', views.staff_member, name="staff"),
