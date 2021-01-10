4a3b36d958e86fc4e02116a5313ed39f59ebf80e
Walsh, Michael P <mwalsh161@gmail.com>
2021-01-10 16:18:02 -0500

    Link customizing committee term via GET param

--- members/views.py
+++ members/views.py
@@ -2,17 +2,21 @@
 from __future__ import unicode_literals
 
 import pytz
+import datetime
+import traceback
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.urls import reverse
 from django.template import loader
+from django.conf import settings
+from django.utils.safestring import mark_safe
 from django.contrib.auth.models import User, Group
 from django.views.generic.edit import FormView
 
 from members.forms import PersonForm, RegistrationForm
-from members.models import Person, get_term_containing
+from members.models import Person, Term, get_term_containing
 from iquise.utils import basic_context, decode_data
 
 
@@ -86,13 +90,51 @@ def staff_register(request, hash_):
 
 def committee(request, name):
     group = get_object_or_404(Group, name__iexact=name)
-    # TODO: make term an GET param
+    # TODO: send full list and make date next/back entirely frontend JS
+    date_str = request.GET.get("date")
     date = timezone.now().date()
+    notifications = []
+    if date_str:
+        try:
+            date_els = [int(item) for item in date_str.split("-")] # Convert to list of ints: [2020, 12, 12]
+            date = datetime.date(*date_els)
+        except:
+            if settings.DEBUG:
+                notifications = [mark_safe(traceback.format_exc().replace("\n", "<br>"))]
+            else: # Don't include any details of backend!
+                notifications = ["Failed to decode '%s', should be 'YYYY-MM-DD'" % date_str]
     term = get_term_containing(date)
+    # Need new query to get surrounding ones (not include equality in comparison should be sufficient)
+    if term: # Remember sorted by newest to oldest
+        next_term = Term.objects.filter(start__gt=term.start).last()
+        previous_term = Term.objects.filter(start__lt=term.start).first()
+    else: # Could still be a future term
+        next_term = Term.objects.filter(start__gte=date).last()
+        previous_term = None
+
+    pos_held = group.committee.get_positions_held(term)
+    if term:
+        start = term.start
+        stop = term.get_end()
+    else:
+        if pos_held: # Better to not call .exists() since we iterate later anyway (django caches)
+            start = pos_held.first().start
+        else:
+            start = date
+        if next_term:
+            stop = next_term.start
+        elif pos_held:
+            stop = pos_held.last().start
+        else:
+            stop = None
     context = {
         'group': group,
-        'pos_held': group.committee.get_positions_held(term),
-        'term': term,
+        'pos_held': pos_held,
+        'start': start,
+        'stop': stop,
+        'next_term': next_term,
+        'previous_term': previous_term,
     }
     context.update(basic_context(request))
+    context["notifications"] = context.get("notifications", []) + notifications
     return render(request, "members/committee.html", context)
