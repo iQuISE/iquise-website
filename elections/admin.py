a3dfc5fe4c0f902505ee36c9a2219bb69d7af40d
Walsh, Michael P <mwalsh161@gmail.com>
2021-03-19 16:20:59 -0400

    add more data/filterables to admin

--- elections/admin.py
+++ elections/admin.py
@@ -5,8 +5,19 @@ from django.contrib import admin
 
 from elections.models import Election, Ballot, Voter, Nominee, Candidate
 
-admin.site.register(Election)
-admin.site.register(Ballot)
-admin.site.register(Voter)
+
+class ElectionAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "nomination_start", "vote_start")
+
+class BallotAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "position_number", "election")
+    list_filter = ("election", )
+
+class VoterAdmin(admin.ModelAdmin):
+    list_filter = ("election", )
+
+admin.site.register(Election, ElectionAdmin)
+admin.site.register(Ballot, BallotAdmin)
+admin.site.register(Voter, VoterAdmin)
 admin.site.register(Nominee)
 admin.site.register(Candidate)
