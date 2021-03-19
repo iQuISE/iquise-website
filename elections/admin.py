497ef2db7c2692d3474e26f6cc8743a937390a5a
Walsh, Michael P <mwalsh161@gmail.com>
2021-03-19 11:56:14 -0400

    Register admin models and add some fields

--- elections/admin.py
+++ elections/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from elections.models import Election, Ballot, Voter, Nominee, Candidate
+
+admin.site.register(Election)
+admin.site.register(Ballot)
+admin.site.register(Voter)
+admin.site.register(Nominee)
+admin.site.register(Candidate)
--- elections/models.py
+++ elections/models.py
@@ -2,11 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # REF: https://github.com/MasonM/django-elect
 # TODO: convert introduction fields to use markdown
+
+def get_current_election():
+    now = timezone.now()
+    election = Election.objects.filter(nomination_start__lte=now).filter(vote_end__gte=now)
+    n = election.count()
+    if n > 1:
+        raise ValueError("Database contains multiple active elections.")
+    if n == 1:
+        return election[0]
+    return None
 
 class Election(models.Model):
     """Represents a single election.
@@ -20,6 +30,11 @@ class Election(models.Model):
     introduction = models.TextField(blank=True,
         help_text="This is printed at the top of the voting page below "+\
         "the header. Enter the text as HTML.")
+    nomination_introduction = models.TextField(blank=True,
+        help_text="This is printed at the top of the nomination page below "+\
+        "the header. Enter the text as HTML.")
+    nomination_start = models.DateTimeField(help_text="Start of nominations")
+    nomination_end = models.DateTimeField(help_text="End of nominations")
     vote_start = models.DateTimeField(help_text="Start of voting")
     vote_end = models.DateTimeField(help_text="End of voting")
     allowed_voters = models.ManyToManyField(User, through="Voter")
@@ -30,15 +45,18 @@ class Election(models.Model):
 class Voter(models.Model):
     """We can store a token to send a unique email to users so we don't require login.
 
-    Once used, this token should be removed to avoid re-use.
+    Once used, this token should be removed to avoid re-use if so desired.
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     election = models.ForeignKey("Election", on_delete=models.CASCADE)
-    token = models.CharField(max_length=255)
+    token = models.CharField(max_length=10) # base64, is more than enough
 
     class Meta:
         # There should be no duplicate tokens (or users) in an election!
         unique_together=(("election", "token"), ("election", "user"))
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Ballot(models.Model):
@@ -55,7 +73,7 @@ class Ballot(models.Model):
     # seats_available = models.PositiveSmallIntegerField(default=1)
 
     def __unicode__(self):
-        return "%s: %s" %(election, description)
+        return "%s: %s" %(self.election, self.description)
 
 class Nominee(models.Model):
     """A nominee is someone considered for a set of ballots, but unconfirmed.
@@ -64,10 +82,11 @@ class Nominee(models.Model):
     committee. This will require all confirmed nominees to have a User account
     which will give us the profile to access basic bio data and a portrait.
     """
-    ballots = models.ManyToManyField(Ballot) # TODO: limit_choices_to=ballots of current election?
+    ballots = models.ManyToManyField(Ballot, help_text="You may select as many as you'd like.") # TODO: limit_choices_to=ballots of current election?
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(help_text="MIT email address if available")
+    nominator = models.ForeignKey(Voter, on_delete=models.CASCADE, related_name="nominees")
 
 class Candidate(models.Model):
     """A candidate is someone that appears on a particular ballot.
