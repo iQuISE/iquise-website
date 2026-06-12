28cc4022feace186453ce40ac7c772dfc71f2416
Carter Turnbaugh <carterturn@tutanota.de>
2026-06-12 17:27:38 -0400

    Updated handler404 signature

--- website/views.py
+++ website/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render
 
 from website.models import *
 
-def handler404(request):
+def handler404(request, exception):
     return render(request, '404.html', status=404)
 
 def index(request):
